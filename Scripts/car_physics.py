feec99bc5baec20a61228de543dd8709c824e073
Favodar <favodar@gmail.com>
2021-02-12 15:01:23 +0100

    changed car_physics to accomodate continuous action spaces, added episode counter to rendering, added rotation-indication in rendering

--- Scripts/car_physics.py
+++ Scripts/car_physics.py
@@ -29,6 +29,8 @@ class Car(Object2D):
         self.acceleration = acceleration
 
         self.rotation = rotation
+        self.rotation_vector_x = np.sin(self.rotation)
+        self.rotation_vector_y = np.cos(self.rotation)
         self.coordinates = np.array([[x_pos, y_pos],])
         self.speed = 0
 
@@ -37,30 +39,28 @@ class Car(Object2D):
         print("maxspeed = " + str(maxspeed))
         print("joint coordinates:" +str(self.coordinates))
     
-    def move(self, gaspedal = 0, rotate = 0):
+    def move(self, gaspedal=0, rotate=0):
 
         # If gaspedal is pushed accelerate, else deccelerate
-        if(gaspedal!=0):
-            self.speed+=(self.acceleration*gaspedal)
-        elif(self.speed>0):
-            self.speed-=(self.acceleration)
+        if(gaspedal > 0):
+            self.speed += (self.acceleration*gaspedal)
+        elif(self.speed > 0):
+            self.speed -= (self.acceleration)
 
-        # Make sure speed is not negative or above max speed or 0: 
+        # Make sure speed is not negative or above max speed or 0:
         if(self.speed > self.maxspeed):
             self.speed = self.maxspeed
         elif(self.speed < 0):
             self.speed = 0
-            
-        if(rotate==0):
-            self.rotation -= self.rotation_step_size
-        elif(rotate==2):
-            self.rotation += self.rotation_step_size
-        elif(rotate!=1):
-            print("invalid rotate value! Must be -1, 0 or 1!")
 
-        self.coordinates[0][0] += self.speed*np.sin(self.rotation)
-        self.coordinates[0][1] += self.speed*np.cos(self.rotation)
-        
+        self.rotation += (rotate-1)*self.rotation_step_size
+
+        self.rotation_vector_x = np.sin(self.rotation)
+        self.rotation_vector_y = np.cos(self.rotation)
+
+        self.coordinates[0][0] += self.speed*self.rotation_vector_x
+        self.coordinates[0][1] += self.speed*self.rotation_vector_y
+
         if(self.border):
             self.coordinates[0][0] %= 100
             self.coordinates[0][1] %= 100
@@ -68,7 +68,7 @@ class Car(Object2D):
 
 
     def get2DpointList(self):
-        return self.coordinates
+        return self.coordinates, [self.rotation_vector_x, self.rotation_vector_y]
 
 
 class Ball(Object2D):
@@ -77,7 +77,7 @@ class Ball(Object2D):
         self.coordinates = np.array([[xPos, yPos],])
 
     def get2DpointList(self):
-        return self.coordinates
+        return self.coordinates, None
 
 class Render:
 
@@ -86,55 +86,54 @@ class Render:
 
     def __init__ (self, object2DList):
         self.objectList = object2DList
-        self.window1 = graphics.GraphWin("window1", 800, 800)
+        self.window1 = graphics.GraphWin("window1", 400, 400)
         self.window1.setCoords(0, 0, 500, 500)
         self.graphicsObjectList = []
-        self.text = graphics.Text(graphics.Point(1, 50), str(""))
+        self.text1 = graphics.Text(graphics.Point(1, 50), str(""))
+        self.text2 = graphics.Text(graphics.Point(1, 50), str(""))
 
     def setObjects (self, object2DList):
         self.objectList = object2DList
 
 
-    
-    def renderFirstFrame(self, reward = 0):
+    def renderFrame(self, reward = 0, episode = 0):
         for gObj in self.graphicsObjectList:
             gObj.undraw()
         self.graphicsObjectList = []
 
-        textpos = graphics.Point(500, 50)
-        self.text.undraw()
-        self.text = graphics.Text(textpos, str(reward))
-        self.text.draw(self.window1)
+        textpos1 = graphics.Point(250, 50)
+        self.text1.undraw()
+        self.text1 = graphics.Text(textpos1, "Reward: " + str(reward))
+        self.text1.draw(self.window1)
+        textpos2 = graphics.Point(250, 450)
+        self.text2.undraw()
+        self.text2 = graphics.Text(textpos2, "Episode: " + str(episode))
+        self.text2.draw(self.window1)
         i = 0
         for obj in self.objectList:
-            points = obj.get2DpointList()
-            for point in points:
-                p1 = graphics.Point(point[0]+200, point[1]+200)
-                c = graphics.Circle(p1, 7)
-                c.setFill(self.colors[i%self.cnumber])
-                c.draw(self.window1)
-                self.graphicsObjectList.append(c)
-            i+=1
+            points, rotation = obj.get2DpointList()
+            if(rotation is not None):
+                for point in points:
+                    p1 = graphics.Point(point[0]+200-3*rotation[0], point[1]+200-3*rotation[1])
+                    p2 = graphics.Point(point[0]+200+3*rotation[0], point[1]+200+3*rotation[1])
+                    c1 = graphics.Circle(p1, 5)
+                    c1.setFill(self.colors[i%self.cnumber])
+                    c1.draw(self.window1)
+                    c2 = graphics.Circle(p2, 5)
+                    c2.setFill(self.colors[i % self.cnumber])
+                    c2.draw(self.window1)
+                    self.graphicsObjectList.append(c1)
+                    self.graphicsObjectList.append(c2)
 
-    def renderFrame(self, reward = 0):
-        for gObj in self.graphicsObjectList:
-            gObj.undraw()
-        self.graphicsObjectList = []
-
-        textpos = graphics.Point(200, 50)
-        self.text.undraw()
-        self.text = graphics.Text(textpos, str(reward))
-        self.text.draw(self.window1)
-        i = 0
-        for obj in self.objectList:
-            points = obj.get2DpointList()
-            for point in points:
-                p1 = graphics.Point(point[0]+200, point[1]+200)
-                c = graphics.Circle(p1, 7)
-                c.setFill(self.colors[i%self.cnumber])
-                c.draw(self.window1)
-                self.graphicsObjectList.append(c)
-            i+=1
+                i+=1
+            else:
+                for point in points:
+                    p1 = graphics.Point(point[0]+200, point[1]+200)
+                    c = graphics.Circle(p1, 7)
+                    c.setFill(self.colors[i % self.cnumber])
+                    c.draw(self.window1)
+                    self.graphicsObjectList.append(c)
+                i += 1
 
         #self.window1.getMouse() # Pause to view result
         # window.close()    # Close window when done
